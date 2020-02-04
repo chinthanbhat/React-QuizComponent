bf622d9e0c345b49933f0ca8f5c284c7d52663d0
Bhat <CB055392@cerner.net>
2020-02-03 21:27:24 -0600

    Take a poll button showing question

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react'
 import './App.css'
 import Quiz from './Quiz.js'
 
+let quizData = require('./quiz_data.json')
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComponent: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      showComponent: true,
+    });
+  }
+
   render() {
     return (
-      <Quiz />
+      <div>
+      <div className="QuizQuestion1">{quizData.quiz_questions[0].instruction_text}</div>
+      <button onClick={this.handleClick}>Take a poll</button>
+      {this.state.showComponent ?
+           <Quiz /> :
+           null
+        }
+      </div>
     )
   }
 }
